6ae2285587e5ae7a209893232e9b3893b433e063
jinhlee19 <jinhlee19@gmail.com>
2023-01-13 23:29:46 +0900

    .

--- pages/404.jsx
+++ pages/404.jsx
@@ -3,7 +3,7 @@ import Layout from "@/components/Layout";
 export default function NotFoundPage() {
   return (
     <Layout title="Page Not Found">
-      <div>
+      <div className="flex justify-center items-center space-y-4">
         <h1>404</h1>
         <h2>Sorry, there is nothing here.</h2>
       </div>
